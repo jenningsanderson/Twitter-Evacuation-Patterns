ceaff3915b32714304fec2e9c901d54f379cc69b
Jennings Anderson <jennings.anderson@colorado.edu>
2014-03-16 10:41:53 -0600

    Building line builder

--- extract_scripts/tracks.rb
+++ extract_scripts/tracks.rb
@@ -9,7 +9,7 @@ require '../tweet_io'    #=> Interface with Mongo
 
 
 #Make a shapefile
-tweet_shape = Tweet_Shapefile.new('SATstudyTime')
+tweet_shape = Tweet_Shapefile.new('line_test')
 tweet_shape.create_point_shapefile
 
 tweets = SandyMongoClient.new #Initiate client
@@ -19,7 +19,7 @@ tweets.query = {"text"=>/SATstudyTime/} #Set the particular query
 gen = tweets.get_tweets_for_plot #Get the tweets
 
 gen.each do |tweet|
-	tweet_shape.add_point(tweet)
+  tweet_shape.add_point(tweet)
 end
 
 tweet_shape.close
