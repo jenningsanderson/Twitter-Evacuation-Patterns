f6fc01f2e2b4288f6868231b8ecda7d164e5e950
Jennings Anderson <jennings.anderson@colorado.edu>
2014-03-20 17:13:16 -0600

    Got reformat date to work

---
+++ parsers/reformat_date.rb
@@ -0,0 +1,35 @@
+'''This script reformats the BSON date in Mongo to an actual date for queries...'''
+
+
+
+
+
+
+
+require '../tweet_io'
+require 'bson'
+require 'date'
+require 'time'
+
+
+
+
+
+
+
+
+if __FILE__ == $0
+  #Connect to Mongo
+  conn = SandyMongoClient.new
+
+  conn.get_all.each do |tweet|
+    unless tweet["created_at"].is_a? Time
+      id        = tweet["_id"]
+      datestamp = DateTime.parse(tweet["created_at"]).to_s
+      timestamp = Time.parse(datestamp).utc
+      #puts timestamp.class
+      #puts timestamp
+      conn.collection.update({"_id" => id },{"$set" => {"created_at" => timestamp}})
+    end
+  end
+end
