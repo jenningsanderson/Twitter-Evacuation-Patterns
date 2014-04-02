690fd589a722ac59898b0982567575b17c0f8fb5
Jennings Anderson <jennings.anderson@colorado.edu>
2014-04-02 14:58:01 -0600

    Added new users

--- config.rb
+++ config.rb
@@ -3,5 +3,7 @@ USERS_TO_IGNORE = [
   {:handle=> 'yatown_news_pm',  :ID_STR=>'468678063'},
   {:handle=> 'yatown_news_am',  :ID_STR=>'468670963'},
   {:handle=> 'wunderground',    :ID_STR=>'15740491'},
-  {:handle=> 'ThinkerInMotion', :ID_STR=>'28499980'}
+  {:handle=> 'ThinkerInMotion', :ID_STR=>'28499980'},
+  {:handle=> 'HEWSweb',         :ID_STR=>'214940738'},
+  {:hanlde=> 'disasteraware',   :ID_STR=>'85636450'}
 ]
