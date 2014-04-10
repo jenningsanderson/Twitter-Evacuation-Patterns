88e5765c43a87ae6a398cdf832ae19461dc8f7c7
Jennings Anderson <jennings.anderson@colorado.edu>
2014-04-09 18:33:51 -0600

    updated this again

--- fileio/write_geojson.rb
+++ fileio/write_geojson.rb
@@ -104,6 +104,7 @@ if __FILE__ == $0
     coords.each_with_index do |coords, i|
       props[:text] = item["tweets"][i]["text"]
       props[:created_at] = item["tweets"][i]["created_at"]
+      props[:place] = item["tweets"][i]["place"]["full_name"]
       geometry = {:type=>"Point", :coordinates=>coords}
       file.write_feature(geometry, props)
     end
