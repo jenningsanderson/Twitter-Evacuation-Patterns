45706ab356c73024e82b7f97fca42fa0be168a41
Jennings Anderson <jennings.anderson@colorado.edu>
2014-08-23 12:20:15 -0600

    prepping more wrangling scripts

--- wrangling_scripts/assign_handles_to_twitterers_collection.rb
+++ wrangling_scripts/assign_handles_to_twitterers_collection.rb
@@ -1,6 +1,11 @@
 '''
 A simple script to assign handles to the users
 '''
+require 'rubygems'
+require 'bundler/setup'
+require 'active_support'
+require 'active_support/deprecation'
+require 'mongo_mapper'
 
 require 'mongo_mapper'
 
@@ -9,14 +14,12 @@ require_relative '../models/tweet'
 
 
 #Static Setup
-MongoMapper.connection = Mongo::Connection.new('epic-analytics.cs.colorado.edu')
+MongoMapper.connection = Mongo::Connection.new#('epic-analytics.cs.colorado.edu')
 MongoMapper.database = 'sandygeo'
 
-Twitterer.where(:handle => "").each_with_index do |user, i|
+Twitterer.where(:handle.in => ["",nil]).each_with_index do |user, i|
   handle = user.tweets.collect{|tweet| tweet["handle"]}.flatten.uniq.join(', ')
   user.handle = handle
-  #puts handle
-  #user.process_geometry
   user.save
 
   if (i%100).zero?
