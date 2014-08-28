ee7d42db1ffc5d6bca9ce5fe6d76f8102075785c
Jennings Anderson <jennings.anderson@colorado.edu>
2014-08-28 11:40:56 -0600

    added the new shelter locations updater

---
+++ wrangling_scripts/update_before_after_shelter_locations.rb
@@ -0,0 +1,46 @@
+#
+# Updating Before & After Shelter locations & GeoCoordinates
+#
+
+require 'mongo_mapper'
+require 'epic-geo'
+
+require_relative '../models/twitterer'
+require_relative '../models/tweet'
+require_relative '../processing/geoprocessing'
+
+#Static Setup
+MongoMapper.connection = Mongo::Connection.new('epic-analytics.cs.colorado.edu')
+MongoMapper.database = 'sandygeo'
+
+
+results = Twitterer.where( 
+	
+	:path_affected => true,
+	:unclassifiable.ne => true,
+	:issue => 1000,
+
+).limit(nil).sort(:tweet_count)
+
+puts "Found #{results.count} users"
+
+results.each_with_index do |user, index|
+
+	user.before_home_cluster = find_before_home(user.clusters_per_day)
+	user.after_home_cluster  = find_after_home(user.clusters_per_day)
+
+	user.cluster_locations[:before_home] = user.cluster_locations[user.before_home_cluster]
+	user.cluster_locations[:after_home]  = user.cluster_locations[user.after_home_cluster]
+	user.issue = 1200
+
+	user.save
+
+	if (index % 100).zero?
+	    print "."
+	elsif (index%1001).zero?
+	    print "#{index}"
+	end
+
+	puts "==================\n\n"
+
+end
