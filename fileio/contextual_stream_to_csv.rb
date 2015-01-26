b29efe54b0841c24f6390fc55f4462556c3e6dbc
Jennings Anderson <jennings.anderson@colorado.edu>
2015-01-26 11:30:48 -0700

    making the writer

--- fileio/contextual_stream_to_csv.rb
+++ fileio/contextual_stream_to_csv.rb
@@ -6,8 +6,7 @@ require_relative '../server_config'
 require_relative '../cloud_export/full_contextual_stream'
 #Because it's meant to be run on the server
 
-include EpicGeo
-include EpicGeo::Writers
+write_directory = './csv_out/'
 
 contextual_stream = FullContextualStreamRetriever.new(
 	start_date:  _start,
@@ -21,6 +20,12 @@ users.each do |handle|
 
 	tweets = contextual_stream.get_full_stream(handle)
 	puts "Total tweets: #{tweets.length}"
+
+	File.open(write_directory+handle+'.csv', "w") do |file|  
+		tweets.each do |tweet|
+			file.write "#{tweet.date}, #{tweet.text}"
+		end
+	}
 
 end
 
