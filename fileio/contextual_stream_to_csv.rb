c40bb8d97ccc4fad8e093b0485555761fd9ac01c
Jennings Anderson <jennings.anderson@colorado.edu>
2015-01-26 11:21:58 -0700

    testing new contextual info

---
+++ fileio/contextual_stream_to_csv.rb
@@ -0,0 +1,26 @@
+include EpicGeo
+include EpicGeo::Writers
+
+_start =  Time.new(2012,10,22)
+_end   =  Time.new(2012,11,14)
+
+#We're running on the server!  here we go!
+require_relative '../server_config'
+require_relative '../cloud_export/full_contextual_stream'
+#Because it's meant to be run on the server
+
+contextual_stream = FullContextualStreamRetriever.new(
+	start_date:  _start,
+	end_date:    _end,
+	root_path:   "/home/kena/geo_user_collection/" )
+
+
+users = ['iKhoiBui']
+
+users.each do |handle|
+
+	tweets = contextual_stream.get_full_stream(handle)
+
+end
+
+puts "Total tweets: #{tweets.length}"
