b32ecac2cbffce6744c6c21a84d2a93923988d97
Jennings Anderson <jennings.anderson@colorado.edu>
2015-04-24 12:33:18 -0500

    sorting by date first

--- datasets/json_to_csv.rb
+++ datasets/json_to_csv.rb
@@ -51,7 +51,9 @@ JSON.parse(File.read('./dataset0.json')).each do |id, tweet|
 	end
 end
 
-sorted = all_tweets.sort_by{|tweet| tweet["user"]}
+sorted_date = all_tweets.sort_by{ sort_by{|tweet| Date.new(tweet["date"])} }
+
+sorted = sorted_date.sort_by{|tweet| tweet["user"]}
 
 CSV.open('all_output.csv', 'wb') do |csv|
 	csv << default_columns+coding_categories
