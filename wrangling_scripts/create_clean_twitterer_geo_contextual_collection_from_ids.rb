1190913dc8df3113747048e699a7f6cac39a7aec
Jennings Anderson <jennings.anderson@colorado.edu>
2015-07-05 16:10:17 -0600

    why no

--- wrangling_scripts/create_clean_twitterer_geo_contextual_collection_from_ids.rb
+++ wrangling_scripts/create_clean_twitterer_geo_contextual_collection_from_ids.rb
@@ -33,6 +33,7 @@ File.readlines('datasets/missing_coded_ids.txt').each_with_index do |line, index
 
   unless id_str.nil?
     keyword_tweet_ids = keyword_tweets.find({'user.id_str' => id_str}).to_a.collect{|x| x["id_str"]}
+    puts keyword_tweet_ids.length
     all_tweets = context.get_full_stream(geo_only=true)
 
     all_tweets.sort_by{|t| t[:Date]}.each do |t|
