2fc399b57ddee5fc9a2ca471e94e19799671dd06
Jennings Anderson <jennings.anderson@colorado.edu>
2015-07-05 13:59:21 -0600

    changing to alphanumeric

--- wrangling_scripts/run_longterm_clustering_analysis.rb
+++ wrangling_scripts/run_longterm_clustering_analysis.rb
@@ -8,7 +8,7 @@ geo  = ARGV[1] || 'gem'
 LIMIT   = 100
 PROCESSES = (ARGV[2] || '2').to_i
 
-alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+alpha = ['0','1','2','3','4','5','6','7','8','9','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q']
 
 res = Parallel.map(alpha.first(PROCESSES)) do |letter|
   #
