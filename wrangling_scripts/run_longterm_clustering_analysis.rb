96e8b0e63c0226c02e550e844b169d1df6d00333
Jennings Anderson <jennings.anderson@colorado.edu>
2015-07-08 21:00:25 -0600

    uploading

--- wrangling_scripts/run_longterm_clustering_analysis.rb
+++ wrangling_scripts/run_longterm_clustering_analysis.rb
@@ -7,6 +7,7 @@ env  = ARGV[0] || 'serverlocal'
 geo  = ARGV[1] || 'gem'
 LIMIT   = 100
 PROCESSES = (ARGV[2] || '2').to_i
+QUERY = (ARGV[3] || 100).to_i
 
 require_relative '../movement_derivation_controller'
 #Initialization:
@@ -16,40 +17,31 @@ runtime = TwitterMovementDerivation.new(
   factory: 'global'
 )
 
+ids = Twitterer.where({tweets: {'$size' => {'$gt' => QUERY}}, unclustered_percentage: nil}).each.collect{|t| t._id}
+puts "Found #{ids.count} ids"
 
-# ids = Twitterer.where({tweets: {'$size' => {'$gt' => 10}}, unclustered_percentage: nil}).each.collect{|t| t._id}
-# puts "Found #{ids.count} ids"
+res = Parallel.map(ids.first(PROCESSES)) do |id|
+  #
+  # Make a new everything for this process; then perhaps even split this into further threads?
+  #
+  # load('movement_derivation_controller.rb')
+  runtime = TwitterMovementDerivation.new(
+    environment: env,
+    geo: geo,
+    factory: 'global'
+  )
 
-# Twitterer.all.each do |user|
-#   if user.tweets.count < 50 and user.tweets.count >
-#     puts user
-#   end
-# end
-
-# res = Parallel.map(ids.first(20)) do |id|
-#   #
-#   # Make a new everything for this process; then perhaps even split this into further threads?
-#   #
-#   # load('movement_derivation_controller.rb')
-#   runtime = TwitterMovementDerivation.new(
-#     environment: env,
-#     geo: geo,
-#     factory: 'global'
-#   )
-#
-#   puts id
-#
-#   user = Twitterer.find(id)
-#   puts user.handle
-#   unless user.tweets.count == 0
-#     puts user.handle
-#     begin
-#       user.process_tweets_to_clusters
-#       user.save
-#     rescue => e
-#       puts "Something bad happened on #{user.handle}"
-#       next
-#     end
-#     puts "-------FINISHED #{user.handle}-----------"
-#   end
-# end
+  user = Twitterer.find(id)
+  puts user.handle
+  unless user.tweets.count == 0
+    puts user.handle
+    begin
+      user.process_tweets_to_clusters
+      user.save
+    rescue => e
+      puts "Something bad happened on #{user.handle}"
+      next
+    end
+    puts "-------FINISHED #{user.handle}-----------"
+  end
+end
