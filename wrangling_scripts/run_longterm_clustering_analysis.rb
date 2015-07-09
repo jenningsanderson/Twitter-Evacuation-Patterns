4ef330a38b19a7d7450779ef90a347650cd1f1f4
Jennings Anderson <jennings.anderson@colorado.edu>
2015-07-08 21:04:21 -0600

    fix that query, watson

--- wrangling_scripts/run_longterm_clustering_analysis.rb
+++ wrangling_scripts/run_longterm_clustering_analysis.rb
@@ -17,7 +17,7 @@ runtime = TwitterMovementDerivation.new(
   factory: 'global'
 )
 
-ids = Twitterer.where({tweets: {'$size' => {'$gt' => QUERY}}, unclustered_percentage: nil}).each.collect{|t| t._id}
+ids = Twitterer.where(tweets_count: {'$gt' => QUERY}, unclustered_percentage: nil).each.collect{|t| t._id}
 puts "Found #{ids.count} ids"
 
 res = Parallel.map(ids.first(PROCESSES)) do |id|
