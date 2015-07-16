b583c08a9e7687c8706135a9b3a6319c84681b51
Jennings Anderson <jennings.anderson@colorado.edu>
2015-07-16 16:09:07 -0600

    upping

--- movement_derivation_controller.rb
+++ movement_derivation_controller.rb
@@ -12,35 +12,33 @@ class TwitterMovementDerivation
 
   require 'time'
 
-  attr_reader :environment, :database, :base_path, :factory, :geo
+  attr_reader :db_env, :epic_geo, :base_path, :geo_factory
 
   def initialize(args)
-    @environment = args[:environment].to_sym || :serverlocal
-    @geo         = args[:geo].to_sym         || :gem
-    @factory     = args[:factory]            || 'global'
+    @db_env      = args[:db_env].to_sym      || :local
+    @epic_geo    = args[:epic_geo]           || :gem
+    @geo_factory = args[:geo_factory].to_sym || :global
     @base_path   = args[:base_path]          || '.' #TODO: this should find whatever the current diretory is
-    puts "Initializing Twitter Movement Derivation environment: #{environment}"
+    puts "Initializing Twitter Movement Derivation with database: #{db_env}"
     post_initialize(args)
   end
 
   def post_initialize(args)
 
-    if environment == :processing and geo != :gem
-      require_relative '/home/jennings/epic-geo/lib/epic_geo.rb'
+    if epic_geo == :gem
+      require 'epic_geo'
     else
-      if geo == :gem
-        require 'epic_geo'
-      elsif env == :local
-        require_relative '/Users/jenningsanderson/Documents/epic-geo/lib/epic_geo.rb'
-      end
+      require_relative epic_geo
     end
 
+    #Load Mongoid
     require 'mongoid'
-    Mongoid.load!("#{base_path}/persistence/mongoid.yml", environment)
+    Mongoid.load!("#{base_path}/persistence/mongoid.yml", db_env)
 
+    #Load our Models
     require 'models/twitterer'
 
-    if factory == 'local'
+    if geo_factory == :local
       $fatory = RGeo::Geographic.projected_factory(projection_proj4: '+proj=utm +zone=18 +datum=NAD27 +units=m +no_defs ')
     else
       $factory = RGeo::Geographic.simple_mercator_factory #The basic factory for web mercator data
@@ -63,7 +61,7 @@ class TwitterMovementDerivation
   }
 
   #Deprecated, will update
-  RISK_LEVELS = {
+  $risk_levels = {
   	10 	=> 	"NYC Zone A",
   	11 	=> 	"NYC Zone B",
   	12 	=> 	"NYC Zone C",
@@ -74,17 +72,21 @@ class TwitterMovementDerivation
 end
 
 if __FILE__ == $0
-  env  = ARGV[0] || 'local'
-  geo  = ARGV[1] || 'gem'
+
+  db_env       = ARGV[0]      || :processing
+  epic_geo     = ARGV[1]      || :gem
+  geo_factory  = ARGV[2]      || :global
+  base_path    = ARGV[3]      || '/Users/jenningsanderson/Documents/Twitter-Evacuation-Patterns'
+
   runtime = TwitterMovementDerivation.new(
-    environment: env,
-    geo: geo,
-    factory: 'global'
+    db_env:   db_env,
+    epic_geo: epic_geo,
+    geo_factory: geo_factory,
+    base_path: base_path
   )
 
-
   puts 'yup'
-  coded_users =  ["GinaBoop21", "4thFloorWalkUp", "acbrush", "1903barisdamci", "aalhaider84", "977wctyJesse", "D_AGOSTINO", "AdieMeshel", "rcrocetti", "acdm", "onacitaveoz", "ccompitiello", "3ltutuykt", "2fritters", "502BIGBLOCK", "JFranxMon", "aby_orozco", "246TiffTiff", "nikkovision", "acdcrocker94", "forero29", "txcoonz", "voudonchilde", "adiesaurus", "abestt", "aaronlugo20", "yogabeth218", "AdamBroitman", "compa_tijero", "37kyle", "12CornersNYC", "ABerneche11", "hatchedit", "aanniemal", "ryryrocketss", "AbdulazizSadeq", "JoeeSmith19", "acordingley", "a13xandraaaa", "WaitingQueen", "danielleleiner", "abr74", "92Hughes92", "brittlizarda", "33amelie", "aidenscott", "5pointbuck", "aceytoso_2", "TravissGraham", "Nikki_DeMarco", "haleyybreen", "abrackin", "DDSethi", "haleighbethhh", "Mac_DA_45", "40Visionz", "b_mazzz", "132Sunshine", "1stFITNESSMC", "CluelessMaven", "adel1196", "aaziz830", "adawood30", "DbLeonor", "bakedtofu", "ActualyAmGeorge", "AdamVanBavel", "workfreelyblog", "HarriBoiii", "brieeellee", "AndeLund", "1Vincent", "Zach_Massari10", "Roze_316", "RedJazz43", "1xr650guy", "lizeeSuX", "4everSeductive", "AmberAAlonzo", "Kessel_Erich2", "adamebnit", "PainFresh6", "according2Drew", "Tyler_Mayer", "Sara_Persiano", "adampdouglas", "ACPressLee", "AdamHedenskog", "Caitles16", "adonatelle", "DJsonatra", "Scott_Gaffney", "GrooDs", "acwelch", "just_teevo", "mynameisluissss", "kcgirl2003"]
+  # coded_users =  ["GinaBoop21", "4thFloorWalkUp", "acbrush", "1903barisdamci", "aalhaider84", "977wctyJesse", "D_AGOSTINO", "AdieMeshel", "rcrocetti", "acdm", "onacitaveoz", "ccompitiello", "3ltutuykt", "2fritters", "502BIGBLOCK", "JFranxMon", "aby_orozco", "246TiffTiff", "nikkovision", "acdcrocker94", "forero29", "txcoonz", "voudonchilde", "adiesaurus", "abestt", "aaronlugo20", "yogabeth218", "AdamBroitman", "compa_tijero", "37kyle", "12CornersNYC", "ABerneche11", "hatchedit", "aanniemal", "ryryrocketss", "AbdulazizSadeq", "JoeeSmith19", "acordingley", "a13xandraaaa", "WaitingQueen", "danielleleiner", "abr74", "92Hughes92", "brittlizarda", "33amelie", "aidenscott", "5pointbuck", "aceytoso_2", "TravissGraham", "Nikki_DeMarco", "haleyybreen", "abrackin", "DDSethi", "haleighbethhh", "Mac_DA_45", "40Visionz", "b_mazzz", "132Sunshine", "1stFITNESSMC", "CluelessMaven", "adel1196", "aaziz830", "adawood30", "DbLeonor", "bakedtofu", "ActualyAmGeorge", "AdamVanBavel", "workfreelyblog", "HarriBoiii", "brieeellee", "AndeLund", "1Vincent", "Zach_Massari10", "Roze_316", "RedJazz43", "1xr650guy", "lizeeSuX", "4everSeductive", "AmberAAlonzo", "Kessel_Erich2", "adamebnit", "PainFresh6", "according2Drew", "Tyler_Mayer", "Sara_Persiano", "adampdouglas", "ACPressLee", "AdamHedenskog", "Caitles16", "adonatelle", "DJsonatra", "Scott_Gaffney", "GrooDs", "acwelch", "just_teevo", "mynameisluissss", "kcgirl2003"]
 
   # Twitterer.where(evacuated: "yes").each do |user|
   #   puts user.handle
